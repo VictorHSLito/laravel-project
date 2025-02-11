71a2bc5d56ac9ec92d16ef5976a0e6ee220f573f
Victor Hugo <victorferrarihugo@hotmail.com>
2025-02-10 21:20:32 -0300

    feat: Implementa as views através dos Controllers

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', [\App\Http\Controllers\AboutUsController::class,'aboutUs']);
+
+Route::get('/contact', [\App\Http\Controllers\ContactController::class,'contact']);
+
+Route::get("/teste", [\App\Http\Controllers\PrincipalController::class,"principal"]);
